6eade6a3fdd0e1b7582ac718bad5f8a4b573094d
buiatuan <devils2tuan@gmail.com>
2022-03-10 16:10:45 +0700

    sửa link header với chi tiet trang chu

--- js/product.js
+++ js/product.js
@@ -1,9 +1,33 @@
-/*
-function showSeach(){
-    var s = document.getElementById("show-seach-product");
-    if(s.style.display=="none"){
-        s.style.display = "block";
+/*function showSeach_1(){
+    var s1 = document.getElementById("step-1");
+    if(s1.style.display=="none"){
+        s1.style.display = "block";
     }else {
-        s.style.display = "none";
+        s1.style.display = "none";
+    }
+}
+function showSeach_2(){
+    var s2 = document.getElementById("step-2");
+    if(s2.style.display=="none"){
+        s2.style.display = "block";
+    }else {
+        s2.style.display = "none";
+    }
+}
+function showSeach_3(){
+    var s3 = document.getElementById("step-3");
+    if(s3.style.display=="none"){
+        s3.style.display = "block";
+    }else {
+        s3.style.display = "none";
+    }
+}
+function showSeach_4(){
+    var s4 = document.getElementById("step-4");
+    if(s4.style.display=="none"){
+        s4.style.display = "block";
+    }else {
+        s4.style.display = "none";
     }
 }*/
+
