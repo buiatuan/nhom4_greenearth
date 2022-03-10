32acc10282909c96e816995baa6e7a0a5ae60b56
buiatuan <devils2tuan@gmail.com>
2022-03-10 12:22:21 +0700

    fix link header

---
+++ js/product.js
@@ -0,0 +1,9 @@
+/*
+function showSeach(){
+    var s = document.getElementById("show-seach-product");
+    if(s.style.display=="none"){
+        s.style.display = "block";
+    }else {
+        s.style.display = "none";
+    }
+}*/
